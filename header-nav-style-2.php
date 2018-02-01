9055f0401500415e0c5e5f90d6a66ec24de2a346
Josh DeSherlia <josh@tegile.com>
2018-02-01 10:21:10 -0700

    Theme changes committed by Gulp-Git - 2/1/2018, 10:21:10 AM

--- header-nav-style-2.php
+++ header-nav-style-2.php
@@ -75,85 +75,10 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<!-- Google Tag Manager -->
-		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PLTHCF"
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2C93LK"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-PLTHCF');</script>
-		<!-- End Google Tag Manager -->
-
-		<!-- OneTag Code Insert -->
-
-		<!-- EXPORTED TAGS --> 
-
-		<!-- Tags for Tegile OneTag --> 
-		 
-		<!-- One Tag Conditional Container: Tegile (5789) | Tegile OneTag (4238) -->
-
-			<script type="text/javascript">
-			var ft_onetag_4238 = {
-				ft_vars:{
-					"ftXRef":"",
-					"ftXValue":"",
-					"ftXType":"",
-					"ftXName":"",
-					"ftXNumItems":"",
-					"ftXCurrency":"",
-					"U1":"",
-					"U2":"",
-					"U3":"",
-					"U4":"",
-					"U5":"",
-					"U6":"",
-					"U7":"",
-					"U8":"",
-					"U9":"",
-					"U10":"",
-					"U11":"",
-					"U12":"",
-					"U13":"",
-					"U14":"",
-					"U15":"",
-					"U16":"",
-					"U17":"",
-					"U18":"",
-					"U19":"",
-					"U20":""
-					},
-				ot_dom:document.location.protocol+'//servedby.flashtalking.com',
-				ot_path:'/container/5789;36181;4238;iframe/?',
-				ot_href:'ft_referrer='+escape(document.location.href),
-				ot_rand:Math.random()*1000000,
-				ot_ref:document.referrer,
-				ot_init:function(){
-					var o=this,qs='',count=0,ns='';
-					for(var key in o.ft_vars){
-						qs+=(o.ft_vars[key]==''?'':key+'='+o.ft_vars[key]+'&');
-					}
-					count=o.ot_path.length+qs.length+o.ot_href+escape(o.ot_ref).length;
-					ns=o.ot_ns(count-2000);
-					document.write('<iframe style="position:absolute; visibility:hidden; width:1px; height:1px;" src="'+o.ot_dom+o.ot_path+qs+o.ot_href+'&ns='+ns+'&cb='+o.ot_rand+'"></iframe>');
-				},
-				ot_ns:function(diff){
-					if(diff>0){
-						var o=this,qo={},
-							sp=/(?:^|&)([^&=]*)=?([^&]*)/g,
-							fp=/^(http[s]?):\/\/?([^:\/\s]+)\/([\w\.]+[^#?\s]+)(.*)?/.exec(o.ot_ref),
-							ro={h:fp[2],p:fp[3],qs:fp[4].replace(sp,function(p1,p2,p3){if(p2)qo[p2]=[p3]})};
-						return escape(ro.h+ro.p.substring(0,10)+(qo.q?'?q='+unescape(qo.q):'?p='+unescape(qo.p)));
-					}else{
-						var o=this;
-						return escape(unescape(o.ot_ref));
-					}
-						}
-				}
-			ft_onetag_4238.ot_init();
-			</script>
-
-			<!-- END: OneTag Code Insert -->
+		<!-- End Google Tag Manager (noscript) -->
 
 			<!-- Drop Google Analytics here -->
 			<!-- end analytics -->
