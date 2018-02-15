695b9c412543e315ceabb7b86f9d388dd8f70773
Josh DeSherlia <josh@tegile.com>
2018-02-15 09:20:00 -0700

    Theme changes committed by Gulp-Git - 2/15/2018, 9:20:00 AM

--- page-standard-new.php
+++ page-standard-new.php
@@ -6,15 +6,28 @@ Template Name: Standard Page - New (White Footer)
 
 <?php get_header("standard-new"); ?>
 			
-			<div id="content">
+			<!-- <div id="content"> -->
 						
-				    <div id="main" role="main">
+				    <!-- <div id="main" role="main"> -->
 					
-						<?php get_template_part( 'parts/loop', 'page' ); ?>
-						<?php get_template_part('core/page-blocks'); ?>
+						<?php //get_template_part( 'parts/loop', 'page' ); ?>
+						<?php //get_template_part('core/page-blocks'); ?>
 					    					
-    				</div> <!-- end #main --> 
+    				<!-- </div> -->
+    				<!-- end #main --> 
     
-			</div> <!-- end #content -->
+			<!-- </div> -->
+			<!-- end #content -->
 
-<?php get_footer("standard-new"); ?>
+			<!-- Testing -->
+			<?php
+				echo '<pre>';
+				$desc = get_post_meta($post,'_su_description');
+				print_r($desc);
+				// print_r(get_post_custom());
+				echo '</pre></body>';
+				return;
+			?>
+			<!-- End Testing -->
+
+<?php //get_footer("standard-new"); ?>
