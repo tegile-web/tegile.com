7cd20d1ec79c4ef74fd6bcea2fb501c18f653ae5
Josh DeSherlia <josh@tegile.com>
2018-02-15 09:27:23 -0700

    Theme changes committed by Gulp-Git - 2/15/2018, 9:27:23 AM

--- page-standard-new.php
+++ page-standard-new.php
@@ -4,31 +4,33 @@ Template Name: Standard Page - New (White Footer)
 */
 ?>
 
-<?php //get_header("standard-new"); ?>
-			
-			<!-- <div id="content"> -->
-						
-				    <!-- <div id="main" role="main"> -->
-					
-						<?php //get_template_part( 'parts/loop', 'page' ); ?>
-						<?php //get_template_part('core/page-blocks'); ?>
-					    					
-    				<!-- </div> -->
-    				<!-- end #main --> 
-    
-			<!-- </div> -->
-			<!-- end #content -->
+<?php get_header("standard-new"); ?>
 
 			<!-- Testing -->
 			<?php
-				echo '<body><pre>';
-				// $desc = get_post_custom();
-				// $desc = get_post_meta(get_the_ID());
 				$desc = get_post_meta(get_the_ID(),'_su_description');
-				print_r($desc);
-				echo '</pre></body>';
-				return;
+
+				if ( (count($desc > 0)) && ($desc[0]) ) {
+					$desc = $desc[0];
+				} else {
+					$desc = "MISSING DESCRIPTION!!!";
+				}
+
+				echo '<script type="text/javascript">console.log("'.$desc.'");</script>';
 			?>
 			<!-- End Testing -->
+			
+			<div id="content">
+						
+				    <div id="main" role="main">
+					
+						<?php get_template_part( 'parts/loop', 'page' ); ?>
+						<?php get_template_part('core/page-blocks'); ?>
+					    					
+    				</div>
+    				<!-- end #main --> 
+    
+			</div>
+			<!-- end #content -->
 
-<?php //get_footer("standard-new"); ?>
+<?php get_footer("standard-new"); ?>
