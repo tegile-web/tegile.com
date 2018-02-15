4c136b28f23b90008efabd5665e59a102f867bb1
Josh DeSherlia <josh@tegile.com>
2018-02-15 09:16:37 -0700

    Theme changes committed by Gulp-Git - 2/15/2018, 9:16:37 AM

--- header-standard-new.php
+++ header-standard-new.php
@@ -97,8 +97,11 @@
 		<!-- Testing -->
 		<pre>
 			<?php
-				print_r(get_post_custom());
+				$desc = get_post_meta($post,'_su_description');
+				print_r($desc);
+				// print_r(get_post_custom());
 				return;
+
 			?>
 		</pre>
 		<!-- End Testing -->
