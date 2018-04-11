1ded85be316e2f3c065468dcb8bed50ab250ad13
Josh DeSherlia <josh@tegile.com>
2018-04-11 15:41:29 -0700

    Theme changes committed by Gulp-Git - 4/11/2018, 3:41:29 PM

--- functions.php
+++ functions.php
@@ -58,9 +58,25 @@ foreach(glob(__DIR__."/taxonomies/*.php") as $filename){
     include $filename;
 }
 
+// Add Custom WP-Cron Intervals
+function add_cron_intervals( $schedules ) {
+    $schedules['five_minutes'] = array(
+        'interval' => 300,
+        'display'  => esc_html__( 'Every Five Minutes' ),
+    );
+ 
+    return $schedules;
+}
+add_filter( 'cron_schedules', 'add_cron_intervals' );
+
 // Import NASPO Management functions
 require_once(get_template_directory().'/assets/functions/naspo-management.php');
-add_action( 'init', 'naspo_page_manager' );
+
+// add_action( 'init', 'naspo_page_manager' );
+add_action( 'naspo_cron', 'naspo_page_manager' );
+if ( ! wp_next_scheduled( 'naspo_cron' ) ) {
+    wp_schedule_event( time(), 'finve_minutes', 'naspo_cron' );
+}
 
 // Define our notices array for use globally
 $notices = array();
