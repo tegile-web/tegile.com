419e7bc679377390139382bf07ce45a929284e64
Josh DeSherlia <josh@tegile.com>
2018-12-10 08:41:03 -0700

    Theme changes committed by Gulp-Git - 12/10/2018, 8:41:03 AM

--- footer-standard-new.php
+++ footer-standard-new.php
@@ -1,36 +1,32 @@
+<?php
+
+	$logo_src = __DIR__ . '/../../uploads/2018/12/western-digital-logo.svg';
+  	$logo = file_get_contents( $logo_src );
+
+?>
+
 					<footer class="footer row-wide lp-footer">
 						<div class="row">
 							<div class="large-2 medium-3 small-6 columns text-left">
 								<ul>
-									<li><a href="<?php echo home_url(); ?>/products/">Products</a></li>
-									<li><a href="<?php echo home_url(); ?>/solutions/">Solutions</a></li>
-									<li><a href="<?php echo home_url(); ?>/resources/">Resources</a></li>
-									<li><a href="<?php echo home_url(); ?>/events/">Events</a></li>
-									<li><a href="<?php echo home_url(); ?>/partners-overview/">Partners</a></li>
-									<li><a href="<?php echo home_url(); ?>/company/">About Us</a></li>
-									<li><a href="<?php echo home_url(); ?>/blog/">Blog</a></li>
+									<li><a href="https://www.westerndigital.com/products/storage-systems">Products</a></li>
+									<li><a href="https://www.westerndigital.com/solutions">Solutions</a></li>
+									<li><a href="https://www.westerndigital.com/company/newsroom?type=Events">Events</a></li>
 								</ul>
 							</div>
 							<div class="large-2 medium-3 small-6 large-push-8 medium-push-6 columns text-right">
 								<ul>
-						            <li><a href="<?php echo home_url(); ?>/flash-storage-introduction/">Flash Introduction</a></li>
-						            <li><a href="<?php echo home_url(); ?>/flash-storage-economic-sense/">Flash Economics</a></li>
+						            <li><a href="https://www.westerndigital.com/partners">Partners</a></li>
+									<li><a href="https://www.westerndigital.com/company">About Us</a></li>
+									<li><a href="https://blog.westerndigital.com/">Blog</a></li>
 			  					</ul>
-								<ul>
-									<li><a href="<?php echo home_url(); ?>/company/contact-us/">Contact Us&nbsp;&nbsp;<i class="fa fa-fw fa-phone no-margin"></i></a></li>
-									<li><a href="<?php echo home_url(); ?>/pricing/">Pricing&nbsp;&nbsp;<i class="fa fa-fw fa-dollar no-margin"></i></a></li>
-									<li><a href="<?php echo home_url(); ?>/support/">Support&nbsp;&nbsp;<i class="fa fa-fw fa-headphones no-margin"></i></a></li>
-									<?php if(is_single('18032')) :?>
-									<?php else :?>
-									<li><a href="<?php echo home_url(); ?>/resource/three-minute-intelliflash-demo/">Demo&nbsp;&nbsp;<i class="fa fa-fw fa-laptop no-margin"></i></a></li>
-									<?php endif;?>
-								</ul>
 							</div>
 							<div class="clear200rem show-for-small-only"></div>
 							<div id="footer-middle" class="large-8 medium-6 small-12 large-pull-2 medium-pull-3 columns text-center">
 								<a href="<?php echo home_url(); ?>" rel="nofollow">
-									<!-- <img class="lp-footer-logo" src="<?php echo home_url(); ?>/wp-content/uploads/2016/11/Tegile-Logo-Black-Flat-Vector.svg"> -->
-									<img class="lp-footer-logo" src="<?php echo home_url(); ?>/wp-content/uploads/2017/09/tegile-wd-logo-black.svg">
+									<span class="lp-footer-logo">
+										<?php echo $logo; ?>
+									</span>
 								</a>
 								<div class="clearfix"></div>
 								<ul class="copyright">
